d471a747d7128c8e26df482bf4ccd19f7216e236
JanuszPa <janusz.parfieniuk@gmail.com>
2013-10-17 19:55:36 +0200

    ModelNotFoundException fix

--- client/src/main/java/edu/mit/cci/roma/client/comm/ModelNotFoundException.java
+++ client/src/main/java/edu/mit/cci/roma/client/comm/ModelNotFoundException.java
@@ -4,7 +4,7 @@ package edu.mit.cci.roma.client.comm;
  * @author: jintrone
  * @date: May 20, 2010
  */
-public class ModelNotFoundException extends Throwable {
+public class ModelNotFoundException extends Exception {
     public ModelNotFoundException(String s) {
         super(s);
     }
